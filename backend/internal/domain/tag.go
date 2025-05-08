3516942db62f63a71d51ccb895bbeff5ddced234
Brian. <109451873+brii26@users.noreply.github.com>
2025-05-09 00:05:00 +0700

    Fix: rename wrong imported module name

--- backend/internal/domain/tag.go
+++ backend/internal/domain/tag.go
@@ -3,9 +3,9 @@ package domain
 import (
 	"context"
 
-	"github.com/alex-guoba/gin-clean-template/internal/dao"
-	"github.com/alex-guoba/gin-clean-template/internal/entity"
-	"github.com/alex-guoba/gin-clean-template/pkg/errcode"
+	"github.com/faawibowo/Tubes2_Gopher/internal/dao"
+	"github.com/faawibowo/Tubes2_Gopher/internal/entity"
+	"github.com/faawibowo/Tubes2_Gopher/pkg/errcode"
 
 	"gorm.io/gorm"
 )
