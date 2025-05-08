38d0c261599392bb66c1926682d60f79d6105227
Brian. <109451873+brii26@users.noreply.github.com>
2025-05-08 23:56:33 +0700

    Feat: add image link attribute on map value

--- backend/pkg/mapping/mapping.go
+++ backend/pkg/mapping/mapping.go
@@ -6,12 +6,20 @@ import (
 	"strings"
 )
 
+// Buat decode dari json
 type Element struct {
-	Name    string     `json:"name"`
-	Recipes [][]string `json:"recipes"`
+	Name      string     `json:"name"`
+	Recipes   [][]string `json:"recipes"`
+	ImageLink string     `json:"link"`
 }
 
-func loadRecipes(filename string) (map[string][2]string, error) {
+// Value dari map
+type Value struct {
+	Recipes   [][2]string
+	ImageLink string
+}
+
+func loadRecipes(filename string) (map[string]Value, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -24,24 +32,29 @@ func loadRecipes(filename string) (map[string][2]string, error) {
 		return nil, err
 	}
 
-	recipeMap := make(map[string][2]string)
+	recipeMap := make(map[string]Value)
 
 	for _, elem := range elements {
 		for _, recipe := range elem.Recipes {
+			first := ""
+			second := ""
+
 			if len(recipe) == 2 {
-				first := strings.TrimSpace(recipe[0])
-				second := strings.TrimSpace(recipe[1])
-
-				if first > second {
-					first, second = second, first
-				}
-
-				key := elem.Name
-				recipeMap[key] = [2]string{first, second}
-			} else if recipe == nil {
-				key := elem.Name
-				recipeMap[key] = [2]string{"", ""}
+				first = strings.TrimSpace(recipe[0])
+				second = strings.TrimSpace(recipe[1])
 			}
+
+			if first > second {
+				first, second = second, first
+			}
+
+			key := elem.Name
+			v, exists := recipeMap[key]
+			if !exists {
+				v = Value{ImageLink: elem.ImageLink}
+			}
+			v.Recipes = append(v.Recipes, [2]string{first, second})
+			recipeMap[key] = v
 		}
 	}
 
