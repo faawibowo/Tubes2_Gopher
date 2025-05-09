7834dd18e923b1128efcf14e94d251ae545a12e0
Brian. <109451873+brii26@users.noreply.github.com>
2025-05-09 07:19:40 +0700

    Chore: restructure file position

---
+++ backend/internal/algorithm/bidirectional/bidirectional.go
@@ -0,0 +1,9 @@
+package bidirectional
+
+import (
+	"github.com/faawibowo/Tubes2_Gopher/pkg/DataStructure/Graph"
+)
+
+func BidirectionalSearch(graphMap map[string]*Graph.Graph, start *Graph.Graph) {
+	println("Starting Biderectional at:", start.Element)
+}
